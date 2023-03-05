eeced6c49f88a4c27aa157e905cc7fc81e140306
Rizqi06 <compoterindo@gmail.com>
2023-03-05 12:59:05 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,17 +41,17 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "RzBot-MD" //namabot kalian
+global.ownername = "Ikyy" //nama kalian
+global.myweb = "https://github.com/RizqiRz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/" //bebas asal jan hapus
+global.myweb2 = "https://guthub.com/RizqiRz" //bebas
+global.email = "rizqiiki@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
+global.premium = ['6283865752867'] //premium user
+global.owner = ["6283865752867", "6283854567705"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283865752867" // nomor wa kalian
 global.ownernomerr = "+"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
